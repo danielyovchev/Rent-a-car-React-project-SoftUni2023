85ed53ee8e60350d303c9daaaee938b0a42f5538
danielyovchev <daniel.yovchev@gmail.com>
2023-12-03 23:35:14 +0200

    About page

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,9 +1,27 @@
-export default function About(){
+import styles from "./About.module.css";
+
+export default function About() {
     return (
-        <>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto optio quidem corporis tempore sapiente, eligendi eaque rem unde laboriosam, beatae eveniet possimus corrupti cumque qui dolorum placeat sunt quasi culpa!</p>
-        <p>Lorem ipsum dolor sit amet consectet</p>
-        </>
-        
+        <div className={styles.aboutUsContainer}>
+            <h1>About React Rent-a-Car</h1>
+            <p>Welcome to React Rent-a-Car, your reliable partner for all your travel needs. We offer a wide range of vehicles to suit any journey, whether you're planning a road trip, a business trip, or just need a temporary ride. Our fleet includes the latest models, all well-maintained and equipped with the latest features to ensure your comfort and safety.</p>
+            <p>Our mission is to provide exceptional service and value to our customers. We pride ourselves on our flexible rental options, competitive pricing, and outstanding customer service. Whether you're a first-time renter or a frequent traveler, we're here to make your car rental experience smooth and enjoyable.</p>
+            <p>Thank you for choosing React Rent-a-Car. We look forward to serving you!</p>
+
+            <p>Since our inception in 2010, React Rent-a-Car has been committed to providing quality vehicles and outstanding service. Our journey began with a small fleet of cars and a vision to make car rental more accessible and hassle-free. Over the years, we've grown significantly, adding more vehicles and services to meet the evolving needs of our customers.</p>
+
+            <h2>Meet Our Team</h2>
+            <p>At the heart of React Rent-a-Car is our dedicated team of professionals. From our friendly customer service representatives to our skilled maintenance crew, every member of our team is committed to ensuring the best experience for our customers. We regularly train our staff to stay up-to-date with the latest industry standards and customer service techniques.</p>
+
+            <h2>Our Commitment to You</h2>
+            <p>Your satisfaction is our top priority. We understand that every customer has unique needs, and we strive to meet those needs with a personalized touch. Whether it's assisting you in choosing the perfect vehicle for your trip or providing flexible rental options, we're here to make your car rental experience seamless and enjoyable.</p>
+
+            <p>We also prioritize your safety and comfort. Each vehicle in our fleet undergoes rigorous maintenance and cleaning procedures to ensure that you have a safe and pleasant journey. In the rare event of a vehicle issue, our 24/7 roadside assistance team is always ready to help.</p>
+
+            <h2>Get in Touch</h2>
+            <p>Have questions or need assistance with your booking? Our customer service team is here to help. Contact us via phone, email, or visit us at our office for personalized assistance. We're always happy to provide recommendations and tips to make your travel experience unforgettable.</p>
+
+            <p>Thank you for considering React Rent-a-Car for your travel needs. We look forward to welcoming you and being a part of your journey!</p>
+        </div>
     );
 }
--- client/src/components/Cars/Cars.jsx
+++ client/src/components/Cars/Cars.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+import { getAll } from "../../services/carService";
+import CarCard from "../CarCard/CarCard";
+
 export default function Cars() {
+    const [cars, setCars] = useState([]);
+    useEffect(() => {
+        getAll()
+            .then(result => setCars(result))
+            .catch(err => console.log(err));
+    }, []);
     return (
-        <p>Cars</p>
+        <div className="carsMain">
+            <h2>Our fleet</h2>
+            {/* {cars.map(car => <CarCard key={car._id} />)} */}
+        </div>
     );
 }
